135ec84edb05c551355c0053902a3a5baa9fe0aa
Jermolene <jeremy@jermolene.com>
2016-09-15 09:18:05 +0100

    Add Google Analytics

---
+++ wiki/tiddlers/content/GoogleAnalytics.js
@@ -0,0 +1,17 @@
+/*\
+
+Google Analytics tracker for Federatial
+
+\*/
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+// Replace the account ID below
+
+// window.addEventListener('load',function() {
+ga('create', 'UA-83905294-1', 'auto');
+ga('send', 'pageview');	
+// },false);
