86ebdf66f5988cc265405e7e4eac6566e59af8a9
Jermolene <jeremy@jermolene.com>
2016-09-15 09:26:03 +0100

    Introduce "ScrollFlyer" animation

---
+++ wiki/tiddlers/system/StaticTemplates/ScrollFlyer.js
@@ -0,0 +1,237 @@
+/*\
+
+Scroll Flyer
+
+A scrolling animation that thumbnails the page content when scrolled to the top
+
+TODO: Make things more configurable
+
+\*/
+
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var duration = 700,
+  	lastScrollPosition = 0,
+	isTicking = false,
+	tiddlerShuffledPositions = [],
+	tiddlerNormalPositions = [],
+	tiddlerByTitle = {},
+	domMenuBar = document.querySelector(".tc-hamburger-menu"),
+	domPageHeader = document.querySelector(".tc-page-heading"),
+	domTiddlers = document.querySelectorAll(".tc-story-river .tc-tiddler-frame");
+
+// Set the opacity of the menu bar and each tiddler to hide them
+domMenuBar.style.visibility = "hidden";
+eachDomTiddler(function(domTiddler,index) {
+	domTiddler.style.visibility = "hidden";
+});
+// Remove id's from tiddlers (to prevent scrolling when the hash changes)
+var anchors = document.querySelectorAll("a[id]");
+for(var index = 0; index < anchors.length; index++) {
+	anchors[index].removeAttribute("id");
+}
+// Attach our event listeners
+window.addEventListener("load",onLoad,false);
+window.addEventListener("scroll",onScroll,false);
+window.addEventListener("resize",onResize,false);
+document.body.addEventListener("click",onClick,false);
+window.addEventListener("hashchange",onHashChange,false);
+measureTiddlers();
+
+function onLoad() {
+	// Measure the starting position of tiddlers
+	measureTiddlers();
+	// Force the scroll animation
+	if(window.scrollY === 0) {
+		lastScrollPosition = window.scrollY;
+		updateScrollAnimation(lastScrollPosition);
+	}
+	// Process any hash portion of the URL
+	onHashChange();
+	// Reset the opacity of the menu bar and each tiddler to display them again
+	domMenuBar.style.visibility = "visible";
+	eachDomTiddler(function(domTiddler,index) {
+		domTiddler.style.visibility = "visible";
+	});
+}
+
+function onScroll(e) {
+	lastScrollPosition = window.scrollY;
+	if(!isTicking) {
+		window.requestAnimationFrame(function() {
+			updateScrollAnimation(lastScrollPosition);
+			isTicking = false;
+		});
+	}
+	isTicking = true;
+}
+
+function onResize() {
+	measureTiddlers();
+	lastScrollPosition = window.scrollY;
+	updateScrollAnimation(lastScrollPosition);
+}
+
+function measureTiddlers() {
+	// Measure the base position for the shuffled tiddlers
+	var shuffleTop = domPageHeader.offsetTop + domPageHeader.offsetHeight + parseInt(window.getComputedStyle(domPageHeader).marginBottom,10) / 2;
+	// Save the current tiddler positions as the end positions
+	eachDomTiddler(function(domTiddler,index) {
+		tiddlerNormalPositions[index] = {
+			l: domTiddler.offsetLeft,
+			x: domTiddler.offsetLeft + domTiddler.offsetWidth/2,
+			y: domTiddler.offsetTop,
+			w: domTiddler.offsetWidth,
+			h: domTiddler.offsetHeight
+		};
+		tiddlerByTitle[domTiddler.getAttribute("data-tiddler-title")] = index;
+	});
+	// Generate the starting positions
+	var scale = 0.27,
+		halfTiddlerWidth = tiddlerNormalPositions[0].w * scale / 2,
+		minX = domPageHeader.offsetLeft + halfTiddlerWidth,
+		maxX = domPageHeader.offsetLeft + domPageHeader.offsetWidth - halfTiddlerWidth;
+	for(var t=0; t<tiddlerNormalPositions.length; t++) {
+		var pos = t/(tiddlerNormalPositions.length - 1);
+		tiddlerShuffledPositions[t] = {
+			x: minX + (maxX - minX) * pos,
+			y: shuffleTop,
+			rotate: 0.2 - pos * 0.4,
+			scale: scale
+		}
+	}
+}
+
+function eachDomTiddler(callback) {
+	for(var index = 0; index < domTiddlers.length; index++) {
+		callback(domTiddlers[index],index);
+	}
+}
+
+function updateScrollAnimation(scrollPos) {
+	// Initialise
+	var animTop = tiddlerNormalPositions[0].y * 0.8, // // Lowest scroll position for the animation
+		pos = Math.min(1,scrollPos/animTop), // Scroll position 0..1
+		easedPos = easeInOutQuad(pos,0,1,1), // Eased scroll position 0..1
+		overEasedPos = easeInOutQuad(-scrollPos/(animTop/7),0,1,1); // Easing used for negative scroll positions
+	// Determine where we are in the animation
+	if(scrollPos < 0 || pos === 0) {
+		// At or above the top of the page, hide the menubar and set classes
+		domMenuBar.style.transform = "translateY(-100%)";
+		document.body.classList.add("scrolled-top");
+		document.body.classList.remove("scrolled-mid");
+		document.body.classList.remove("scrolled-bottom");
+	} else if(pos === 1) {
+		// Show the menu bar at the bottom of the page
+		domMenuBar.style.transform = "none";
+		document.body.classList.remove("scrolled-top");
+		document.body.classList.remove("scrolled-mid");
+		document.body.classList.add("scrolled-bottom");
+	} else {
+		// Animate in between
+		domMenuBar.style.transform = "translateY(-" + Math.min((1 - pos) * 100,100) + "%)";
+		document.body.classList.remove("scrolled-top");
+		document.body.classList.add("scrolled-mid");
+		document.body.classList.remove("scrolled-bottom");
+	}
+	// Move each tiddler to the right position
+	eachDomTiddler(function(domTiddler,index) {
+		var shuffled = tiddlerShuffledPositions[index],
+			normal = tiddlerNormalPositions[index],
+			x, y, s, r, transform;
+		if(scrollPos > 0) {
+			// Scroll
+			x = (shuffled.x - normal.x) * (1 - easedPos);
+			y = (shuffled.y - normal.y) * (1 - easedPos);
+			s = 1 + (shuffled.scale - 1) * (1 - easedPos);
+			r = shuffled.rotate * (1 - easedPos);
+		} else {
+			// Overscroll
+			x = shuffled.x - normal.x;
+			y = shuffled.y - normal.y;
+			s = shuffled.scale * (1 - overEasedPos);
+			r = shuffled.rotate;
+		}
+		transform = "translateX(" + x + "px) translateY(" + y + "px) scale(" + s + ") rotate(" + r + "rad)";
+		domTiddler.style.transform = transform;
+	});
+}
+
+function onClick(e) {
+	var domTiddler;
+	// Check for clicking on a local link with the same location hash
+	domTiddler = e.target;
+	while(domTiddler && ((domTiddler.tagName || "").toLowerCase() !== "a")) {
+		domTiddler = domTiddler.parentNode;
+	}
+	if(domTiddler && document.location.toString() === domTiddler.href.toString()) {
+		// Manually trigger the scroll; links to other location hashes within this document will tragger a hashchange event
+		scrollToTiddler(domTiddler.href.toString().split("#")[1]);
+	}
+	// Check for clicking on a thumbnail
+	domTiddler = e.target;
+	if(!document.body.classList.contains("scrolled-bottom")) {
+		while(domTiddler && (!domTiddler.classList || !domTiddler.classList.contains("tc-tiddler-frame"))) {
+			domTiddler = domTiddler.parentNode;
+		}
+		if(domTiddler) {
+			navigateTo(domTiddler.getAttribute("data-tiddler-title"));
+			e.preventDefault();
+			return false;
+		}		
+	}
+}
+
+function navigateTo(title) {
+	if(window.location.hash.substr(1) === title) {
+		// If the location hash is already correct, just scroll
+		scrollToTiddler(title);
+	} else {
+		// If the location hash is different, force the scroll by changing it
+		window.location.hash = "#" + title;
+	}
+}
+
+function onHashChange() {
+	scrollToTiddler(window.location.hash.substr(1));
+}
+
+function scrollToTiddler(title) {
+	var index = tiddlerByTitle[title],
+		scrollPos = 0;
+	if(title in tiddlerByTitle) {
+		scrollPos = tiddlerNormalPositions[index].y - 100;
+	}
+	scrollTo(scrollPos);
+}
+
+function scrollTo(targetPos) {
+	var startPos = window.scrollY,
+		timeStart,
+		timeElapsed;
+	window.requestAnimationFrame(function(time) {
+		timeStart = time;
+		scrollStep(time);
+	});
+	function scrollStep(time) {
+		timeElapsed = time - timeStart;
+		if(timeElapsed < duration) {
+			var scrollPos = startPos + (targetPos - startPos) * easeInOutQuad(timeElapsed / duration);
+			window.scrollTo(0, scrollPos);
+			window.requestAnimationFrame(scrollStep)
+		} else {
+			window.scrollTo(0, targetPos);
+		}
+	}
+}
+
+// Thanks to https://github.com/gre/bezier-easing
+function easeInOutQuad(t) {
+	return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+}
+
+})();
